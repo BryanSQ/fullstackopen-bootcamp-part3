845fdb4071cdfe92d3a35b38416415918ef45c48
Bryan Josue Sandí Quirós <brysq@estudiantec.cr>
2023-12-17 17:32:15 -0600

    linter

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
-  .then(res => {
-    console.log('Connected to MongoDB');
+  .then(() => {
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB', error.message);
+    console.log('error connecting to MongoDB', error.message)
   })
 
-morgan.token('body', (req, res) => {
+morgan.token('body', (req) => {
   return JSON.stringify(req.body)
 })
 const format = ':method :url :status :res[content-length] :response-time ms :body'
@@ -45,28 +45,6 @@ app.use(cors())
 
 // this has to be the last loaded middleware.
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 // 3.1
 app.get('/api/persons', (req, res) => {
@@ -79,38 +57,38 @@ app.get('/api/persons', (req, res) => {
 app.get('/info', (req, res, next) => {
   const date = new Date()
   Person.find({})
-  .then(returnedPeople => {
-    const countStat = `Phonebook has info for ${returnedPeople.length} people`
-    const info = `<p>${countStat}</p>\
-    <p>${date}</p>`
-    res.send(info)
-  })
-  .catch(error => next(error))
+    .then(returnedPeople => {
+      const countStat = `Phonebook has info for ${returnedPeople.length} people`
+      const info = `<p>${countStat}</p>\
+      <p>${date}</p>`
+      res.send(info)
+    })
+    .catch(error => next(error))
 })
 
 //3.3
 app.get('/api/persons/:id', (req, res, next) => {
   const id = req.params.id
-  console.log(id);
+  console.log(id)
   Person.findById(id)
-  .then(person => {
-    if (person){
-      res.json(person)
-    }
-    else{
-      res.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        res.json(person)
+      }
+      else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 // 3.4 | 3.15
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-  .then(result => {
-    res.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
@@ -121,7 +99,7 @@ app.post('/api/persons', (req, res, next) => {
   // 3.6
   if (!body.name || !body.number){
     return res.status(400).json({
-      error: "Missing info"
+      error: 'Missing info'
     })
   }
   
@@ -131,10 +109,10 @@ app.post('/api/persons', (req, res, next) => {
   })
   
   newPerson.save()
-  .then(saved => {
-    res.json(saved)
-  })
-  .catch(error => next(error))
+    .then(saved => {
+      res.json(saved)
+    })
+    .catch(error => next(error))
   
 })
 
@@ -147,11 +125,11 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: body.number
   }
   
-  Person.findByIdAndUpdate(id, person, {new: true})
-  .then(updated => {
-    res.json(updated)
-  })
-  .catch(error => next(error))
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then(updated => {
+      res.json(updated)
+    })
+    .catch(error => next(error))
 })
 
 app.use(errorHandler)
@@ -159,5 +137,5 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`); 
+  console.log(`Listening on http://localhost:${PORT}`)
 })
